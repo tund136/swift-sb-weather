b84a80b134928be6d787cfd93e8052a39a0f3dec
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-18 21:43:52 +0700

    Create the project

---
+++ SB-MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB-MyWeather
+//
+//  Created by Danh Tu on 18/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
