1fed0ef0a0a170ef73b8f5baa59d8bd20535907d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-18 21:55:32 +0700

    Register 2 cells

--- SB-MyWeather/ViewController.swift
+++ SB-MyWeather/ViewController.swift
@@ -12,13 +12,32 @@ import UIKit
 // Custom Cell: Collection View
 // API - Request to get the data
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var table: UITableView!
+    
+    var models = [Weather]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Register 2 cells
+        
+        table.delegate = self
+        table.dataSource = self
     }
-
-
+    
+    // Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
 
+struct Weather {
+    
+}
