635b0a569097cc7f5fb7382535aa3a09739aa19b
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-18 21:57:42 +0700

    Create HourlyTableViewCell and WeatherTableViewCell Cocoa Touch Class with also create XIB file

---
+++ SB-MyWeather/HourlyCollectionCell/HourlyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HourlyTableViewCell.swift
+//  SB-MyWeather
+//
+//  Created by Danh Tu on 18/10/2021.
+//
+
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SB-MyWeather/WeatherCell/WeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherTableViewCell.swift
+//  SB-MyWeather
+//
+//  Created by Danh Tu on 18/10/2021.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
